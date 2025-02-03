de1a6935b681e36b8513034c31db1baa8a5efb20
ShibasakiTakahiro <reserve.shiba@gmail.com>
2025-02-03 11:52:57 +0000

    instance再構築後

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def recommend():
         return render_template('recommend.html', track=None, error=error_message)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
